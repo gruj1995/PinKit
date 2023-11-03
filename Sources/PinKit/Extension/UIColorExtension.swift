840dd50ce8c4d6c2d83f8dea53ff8476a5ee1d0f
pinyili <gruj1995@gmail.com>
2023-11-03 09:48:44 +0800

    Feat: 新增部分Extension

--- Sources/PinKit/Extension/UIColorExtension.swift
+++ Sources/PinKit/Extension/UIColorExtension.swift
@@ -41,4 +41,20 @@ public extension UIColor {
             alpha: alpha
         )
     }
+
+    var hexString: String {
+        var r: CGFloat = 0
+        var g: CGFloat = 0
+        var b: CGFloat = 0
+        var a: CGFloat = 0
+
+        self.getRed(&r, green: &g, blue: &b, alpha: &a)
+
+        return String(
+            format: "%02X%02X%02X",
+            Int(r * 0xff),
+            Int(g * 0xff),
+            Int(b * 0xff)
+        )
+    }
 }
--- Sources/PinKit/Extension/UITableViewExtension.swift
+++ Sources/PinKit/Extension/UITableViewExtension.swift
@@ -9,24 +9,24 @@ import UIKit
 
 public extension UITableView {
     /// 滾動到頂部
-    func scrollToTop(isAnimated: Bool) {
+    func scrollToTop(animated: Bool) {
         let indexPath = IndexPath(row: 0, section: 0)
-        scrollToIndexPath(indexPath, position: .top, isAnimated: isAnimated)
+        scrollToIndexPath(indexPath, position: .top, animated: animated)
     }
 
     /// 滾動到底部
-    func scrollToBottom(isAnimated: Bool) {
+    func scrollToBottom(animated: Bool) {
         let lastSection = numberOfSections - 1
         let lastRow = numberOfRows(inSection: lastSection) - 1
         let indexPath = IndexPath(row: lastRow, section: lastSection)
-        scrollToIndexPath(indexPath, position: .bottom, isAnimated: isAnimated)
+        scrollToIndexPath(indexPath, position: .bottom, animated: animated)
     }
 
     /// 滾動到指定位置
-    func scrollToIndexPath(_ indexPath: IndexPath, position: UITableView.ScrollPosition, isAnimated: Bool) {
+    func scrollToIndexPath(_ indexPath: IndexPath, position: UITableView.ScrollPosition, animated: Bool) {
         DispatchQueue.main.async {
             if self.hasRowAtIndexPath(indexPath: indexPath) {
-                self.scrollToRow(at: indexPath, at: position, animated: isAnimated)
+                self.scrollToRow(at: indexPath, at: position, animated: animated)
             }
         }
     }
